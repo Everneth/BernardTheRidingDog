6d9e50cc68ff5a63f3aa14418a1fd33ddc4040f9
Mike White <mike.wildcats@gmail.com>
2022-08-03 16:23:05 -0500

    bernard adopted

---
+++ BernardTheRidingDog/Services/CommandHandlerService.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BernardTheRidingDog.Services
+{
+    public class CommandHandlerService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly ConfigService _config;
+        private readonly InteractionService _commands;
+        private IServiceProvider _services;
+
+        public CommandHandlerService(IServiceProvider services, DiscordSocketClient client, 
+            ConfigService config, InteractionService commands)
+        {
+            _client = client;
+            _config = config;
+            _services = services;
+            _commands = commands;
+
+            _client.Ready += RegisterCommands;
+
+            _client.InteractionCreated += InteractionCreated;
+            _commands.SlashCommandExecuted += SlashCommandExecutedAsync;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetExecutingAssembly(), _services);
+        }
+
+        private async Task RegisterCommands()
+        {
+            try
+            {
+                await _commands.RegisterCommandsToGuildAsync(_config.BotConfig.GuildId);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+            _client.Ready -= RegisterCommands;
+        }
+
+        private async Task InteractionCreated(SocketInteraction interaction)
+        {
+            // I am not sure if bot's can trigger interactions/slash commands, but if they can we want to ignore them
+            if (interaction.User.IsBot) return;
+
+            var context = new SocketInteractionContext(_client, interaction);
+            await _commands.ExecuteCommandAsync(context, _services);
+        }
+
+        private async Task SlashCommandExecutedAsync(SlashCommandInfo command, IInteractionContext context, Discord.Interactions.IResult result)
+        {
+            // the command was successful, we don't care about this result, unless we want to log that a command succeeded.
+            if (result.IsSuccess) return;
+
+            await context.Interaction.RespondAsync(result.ErrorReason, ephemeral: true);
+        }
+    }
+}
---
+++ BernardTheRidingDog/Services/ConfigService.cs
@@ -0,0 +1,25 @@
+﻿using Discord.WebSocket;
+using BernardTheRidingDog.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BernardTheRidingDog.Services
+{
+    public class ConfigService
+    {
+        private DiscordSocketClient _client;
+        private DataService _data;
+        private IServiceProvider _services;
+        public BotConfig BotConfig { get; set; }
+
+        public ConfigService(IServiceProvider services)
+        {
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _data = services.GetRequiredService<DataService>();
+            _services = services;
+            BotConfig = _data.Load("config", BotConfig);
+        }
+    }
+}
